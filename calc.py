f4dd7b66810514e6f410b057c3438ca252c28e01
yeonjh0214 <jh.yeon0214@gmail.com>
2024-04-03 02:10:04 +0900

    finished calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,22 @@
 def main():
-
-def add():
-
-def divide():
+    print("Let's implement division. Type two numbers for x and y")
     
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+    
+    
+def add(a, b):
+    return a + b
+    
+    
+def divide(a, b):
+    if b==0:
+        print("Error: cannot divide by zero.")
+    else:
+        return a/
+    
+
+if __name__ == "__main__":
+    main()
